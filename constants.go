4a2a4541cd23b86bc8ec035c1d7943c4f0824471
Simie CC <cscxcs@gmail.com>
2019-01-11 13:28:39 +0800

    :sparkles: Add function RedrawWindow

--- constants.go
+++ constants.go
@@ -3101,3 +3101,19 @@ const (
 	TPM_VERNEGANIMATION uint32 = 0x2000
 	TPM_VERPOSANIMATION uint32 = 0x1000
 )
+
+// RedrawWindow() flags  https://source.winehq.org/source/include/winuser.h
+const (
+	RDW_INVALIDATE      = 0x0001
+	RDW_INTERNALPAINT   = 0x0002
+	RDW_ERASE           = 0x0004
+	RDW_VALIDATE        = 0x0008
+	RDW_NOINTERNALPAINT = 0x0010
+	RDW_NOERASE         = 0x0020
+	RDW_NOCHILDREN      = 0x0040
+	RDW_ALLCHILDREN     = 0x0080
+	RDW_UPDATENOW       = 0x0100
+	RDW_ERASENOW        = 0x0200
+	RDW_FRAME           = 0x0400
+	RDW_NOFRAME         = 0x0800
+)
