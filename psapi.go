1036181a82b61d80028c6653c092f88843fe41f8
Simie CC <cscxcs@gmail.com>
2020-05-19 08:57:29 +0800

    Add GetModuleFileNameEx()

--- psapi.go
+++ psapi.go
@@ -12,7 +12,8 @@ import (
 var (
 	modpsapi = syscall.NewLazyDLL("psapi.dll")
 
-	procEnumProcesses = modpsapi.NewProc("EnumProcesses")
+	procEnumProcesses       = modpsapi.NewProc("EnumProcesses")
+	procGetModuleFileNameEx = modpsapi.NewProc("GetModuleFileNameExW")
 )
 
 func EnumProcesses(processIds []uint32, cb uint32, bytesReturned *uint32) bool {
@@ -22,4 +23,24 @@ func EnumProcesses(processIds []uint32, cb uint32, bytesReturned *uint32) bool {
 		uintptr(unsafe.Pointer(bytesReturned)))
 
 	return ret != 0
+}
+
+// Retrieves the fully qualified path for the file containing the specified module.
+// https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulefilenameexw
+func GetModuleFileNameEx(hProcess HANDLE, hModule HMODULE) (string, DWORD) {
+
+	bufLen := 1024
+	buf := make([]byte, bufLen)
+
+	ret, _, _ := procGetModuleFileNameEx.Call(
+		uintptr(hProcess),
+		uintptr(hModule),
+		uintptr(unsafe.Pointer(&buf)),
+		uintptr(bufLen))
+
+	if ret > 0 {
+		return string(buf), DWORD(ret)
+	} else {
+		return "", DWORD(ret)
+	}
 }
